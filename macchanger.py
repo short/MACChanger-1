d006364c29698c62006cd55068978c726ea32825
Kristaps Kozlovskis <108718585+thenexx@users.noreply.github.com>
2022-10-11 21:02:15 +0300

    Update macchanger.py

--- macchanger.py
+++ macchanger.py
@@ -2,6 +2,22 @@
 
 import subprocess
 import optparse
+import re
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+
+    parser.add_option("-i", "--interface", dest="interface", help="Specify the interface to change it's MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="Specify the new MAC address")
+
+    (options, arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a MAC address, use --help for more info.")
+    return options
 
 
 def change_mac(interface, new_mac):
@@ -12,12 +28,23 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "up"])
 
 
-parser = optparse.OptionParser()
+def get_current_mac(interface):
+    ifconfig = subprocess.check_output(["ifconfig", interface])
+    mac_regex = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig)
 
-parser.add_option("-i", "--interface", dest="interface", help="Specify the interface to change it's MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="Specify the new MAC address")
+    if mac_regex:
+        return mac_regex.group(0)
+    else:
+        print("[-] Could not read MAC address.")
 
-(options, arguments) = parser.parse_args()
 
-change_mac(options.interface, options.new_mac)
+get_options = get_arguments()
+current_mac = get_current_mac(get_options.interface)
+print("Current MAC address is " + str(current_mac))
+change_mac(get_options.interface, get_options.new_mac)
 
+current_mac = get_current_mac(get_options.interface)
+if current_mac == get_options.new_mac:
+    print("[+] MAC address was successfully changed to " + str(current_mac))
+else:
+    print("[-] MAC address did not get changed.")
