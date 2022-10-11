b51d569854ff37a2c5648711ecc6031a482f399c
Kristaps Kozlovskis <108718585+thenexx@users.noreply.github.com>
2022-10-11 18:52:04 +0300

    Added code

---
+++ macchanger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Specify the interface to change it's MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="Specify the new MAC address")
+
+(options, arguments) = parser.parse_args()
+
+change_mac(options.interface, options.new_mac)
+
